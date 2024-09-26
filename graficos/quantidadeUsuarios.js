af37681535ca34c350fa21e3cbc84f1f0a503e17
Matheus Ramos de Lima <105288326+MatheusRamosDeLima-EM@users.noreply.github.com>
2024-09-26 12:42:10 -0300

    aula 10

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js";
+import { getCSS, tickConfig } from "./common.js"
 
 async function fnQuantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -23,7 +23,34 @@ async function fnQuantidadeUsuarios() {
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+          text: 'Redes sociais com mais usuários no mundo',
+           x: 0,
+           font: {
+               color: getCSS('--primary-color'),
+               family: getCSS('--font'),
+               size: 30
+           }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+        }
+      }
     }
     
     const grafico = document.createElement('div')
